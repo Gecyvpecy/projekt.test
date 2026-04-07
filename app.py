33b015070bbe2b64e528272a4c475e0ed50b6830
Gecyvpecy <marttincz2@gmail.com>
2026-04-07 18:29:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,4 @@ def ai_advisor():
 
 if __name__ == '__main__':
     # 3. Port - aplikace naslouchá na portu z proměnné PORT (default 5000)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+  app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
